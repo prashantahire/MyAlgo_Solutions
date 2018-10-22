5fc85ea71e9db06e1f89c85b8e9c55afc0bf657c
Prashant Ahire <33499614+prashantahire@users.noreply.github.com>
2018-10-22 15:24:26 -0700

    Bubble sort algo end to end solution

--- src/my/algo/solution/BubbleSort.java
+++ src/my/algo/solution/BubbleSort.java
@@ -1,10 +1,15 @@
+
+
 package my.algo.solution;
+
+
+import java.util.Random;
+
 
 /**
  * Bubble sort time complexity O(n^2)
  * 
  * @author prashant
- *
  */
 public class BubbleSort {
 
@@ -20,5 +25,29 @@ public class BubbleSort {
         }
         System.out.println("Buble Sort output: ");
         printArray(array);
+    }
+
+    public int[] getArray(int size) {
+        Random random = new Random();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(size);
+        }
+        printArray(array);
+        return array;
+    }
+
+    public void printArray(int array[]) {
+        for (int e : array) {
+            System.out.print(e + ",");
+        }
+        System.out.println("");
+        System.out.println("=============");
+    }
+
+    public static void main(String... args) {
+        InsertionSort sort = new InsertionSort();
+        int array[] = sort.getArray(10);
+        sort.insertionSort(array);
     }
 }
