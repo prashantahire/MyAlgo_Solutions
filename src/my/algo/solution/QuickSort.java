efc305c7b37493f901d10eae1d71e5bd43beb4dd
Prashant Ahire <33499614+prashantahire@users.noreply.github.com>
2018-10-31 15:51:58 -0700

    Rec call

--- src/my/algo/solution/QuickSort.java
+++ src/my/algo/solution/QuickSort.java
@@ -4,6 +4,22 @@ package my.algo.solution;
 
 
 public class QuickSort {
+    
+    /**
+     * Uses partitioning to sort.... Best case T(n) = O(nlogn) Worst case T(n) = O(n^2)
+     * 
+     * @param array
+     * @param low
+     * @param high
+     */
+    public void quickSort(int array[], int low, int high) {
+        if (low < high) {
+            int partitionPosition = partition(array, low, high);
+            quickSort(array, low, partitionPosition - 1);
+            quickSort(array, partitionPosition + 1, high);
+        }
+    }
+    
     /**
      * This method consider mid of the array as PIVOT and returns the partitioning position for the selected pivot. T(n)
      * = O(n) Partition Rule: Left elements <= Pivot and right elements > Pivot
