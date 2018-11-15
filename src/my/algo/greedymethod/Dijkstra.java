f3ab83c83a416a8c8d4dc79f09c570f307bc34f7
Prashant Ahire <33499614+prashantahire@users.noreply.github.com>
2018-11-14 16:52:29 -0800

    Dijkstra single source shortest path

---
+++ src/my/algo/greedymethod/Dijkstra.java
@@ -0,0 +1,31 @@
+package my.algo.greedymethod;
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+//Works with directed/non-directed non negative graph.
+//Time complexity with array O(V^2 +E )... With PQ (V*logV+E*logV)
+public class Dijkstra {
+class WGraph {
+    int vertices;
+    List<WNode>[] adj;
+
+    @SuppressWarnings("unchecked")
+    WGraph(int v) {
+        vertices = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; i++) {
+            adj[i] = new LinkedList<>();
+        }
+
+    }
+
+    void addEdge(int u, int v, int wt) {
+        adj[u].add(new WNode(v, wt));
+    }
+}
+
+
+}
