e584450edfcae8a50faf19c21b19d583b3f86fc2
Prashant Ahire <33499614+prashantahire@users.noreply.github.com>
2018-11-26 16:20:39 -0800

    Final

--- src/my/algo/greedymethod/BellmanFord.java
+++ src/my/algo/greedymethod/BellmanFord.java
@@ -1,8 +1,11 @@
 
+
 package my.algo.greedymethod;
 
 
-public class BellmanFord {    public void bellmanFordShortestPath(WGraph graph) {
+public class BellmanFord {
+
+    public void bellmanFordShortestPath(WGraph graph) {
 
         int distance[] = new int[graph.vertices];
         int predecessor[] = new int[graph.vertices];
@@ -50,4 +53,34 @@ public class BellmanFord {    public void bellmanFordShortestPath(WGraph graph) 
             System.out.println(" " + i + " -> " + predecessor[i] );
         }
     }
+    
+    
+    public static void main(String ...args) {
+        
+        WGraph graph = new WGraph(5);
+        graph.addEdge(0, 1, 10);
+        graph.addEdge(0, 2, 3);
+        graph.addEdge(1, 2, 1);
+        graph.addEdge(2, 1, 4);
+        graph.addEdge(1, 3, 2);
+        graph.addEdge(2, 3, 8);
+        graph.addEdge(2, 4, 2);
+        graph.addEdge(4, 3, 9);
+        graph.addEdge(3, 4, 7);
+        new BellmanFord().bellmanFordShortestPath(graph);
+        
+        WGraph graph1 = new WGraph(6);
+        graph1.addEdge(0, 1, -1);
+        graph1.addEdge(0, 2, 4);
+        graph1.addEdge(1, 2, 3);
+        graph1.addEdge(1, 3, 2);
+        graph1.addEdge(1, 4, 2);
+        graph1.addEdge(3, 1, 1);
+        graph1.addEdge(4, 3, -3);
+        graph1.addEdge(4, 5, 3);
+
+        new BellmanFord().bellmanFordShortestPath(graph1);
+
     }
+}
+
