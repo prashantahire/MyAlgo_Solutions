c43fa348a6675f45dc9739561fcfe9c69e2bb0d9
Prashant Ahire <33499614+prashantahire@users.noreply.github.com>
2018-11-28 14:59:21 -0800

    algo impl

--- src/my/algo/graph/BFS.java
+++ src/my/algo/graph/BFS.java
@@ -17,6 +17,52 @@ public class BFS {
      */
     public void bfsTraversal(int startNode, Graph graph) {
 
+
+        System.out.println("Start Node :" + startNode);
+        // keep track of visited nodes and level of node
+        int[] visited = new int[graph.vertices];
+
+        // to keep track of parent node for current node
+        int[] parent = new int[graph.vertices];
+
+        // initialize with -1 to indicate not visited
+        for (int i = 0; i < graph.vertices; i++) {
+            visited[i] = -1;
+            parent[i] = -1;
+        }
+
+        // to keep track of next adjacent node to visit for given node
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        // to keep track of level
+        int level = 0;
+        // Start algo implementation
+
+        // initialize for start node
+        queue.add(startNode);
+        visited[startNode] = level;
+        level++;
+
+        while (!queue.isEmpty()) {
+
+            int node = queue.poll();
+
+            List<Integer> adjacentNodes = graph.adj[node];
+            for (Integer adjNode : adjacentNodes) {
+                if (visited[adjNode] == -1) {
+                    visited[adjNode] = level;
+                    parent[adjNode] = node;
+                    queue.add(adjNode);
+                    System.out.println(" Visited " + adjNode);
+                }
+            }
+            level++;
+        }
+        System.out.println("Level of tree " + level);
+        for (int i = 0; i < parent.length; i++) {
+            System.out.println("Node :" + i + " " + " Parent node: " + parent[i]);
+        }
+    
  
     }
 }
