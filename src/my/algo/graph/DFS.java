4943a94a9ccabae8c8964e92689809da6e8f61f3
Prashant Ahire <33499614+prashantahire@users.noreply.github.com>
2018-11-30 12:46:05 -0800

    Update DFS.java

--- src/my/algo/graph/DFS.java
+++ src/my/algo/graph/DFS.java
@@ -1,1 +1,30 @@
 
+package my.algo.graph;
+
+import java.util.Stack;
+
+//Assumption: Graph nodes are int. Need to change data-structure based on type of node
+public class DFS {
+    public void dfsStack(Graph graph) {
+        
+        Stack<Integer> dfsStack = new Stack<>();
+        int visited[] = new int[graph.vertices];
+        //push start vertex
+        
+        
+        for(int i=0; i<graph.vertices; i++) {
+            if(visited[i] != 1)
+                   dfsStack.push(i);
+            while(!dfsStack.isEmpty()) {
+                int temp = dfsStack.pop();
+                visited[temp] = 1;
+                System.out.println("Visited : "+temp);
+                for(int adjNode : graph.adj[temp]) {
+                    if(visited[adjNode] != 1) {
+                        dfsStack.push(adjNode);
+                    }
+                }
+            }            
+        }
+    }
+}
