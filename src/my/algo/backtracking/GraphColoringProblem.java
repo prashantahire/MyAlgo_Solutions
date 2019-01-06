b3bd50550940b7947ab270902abb6fcc9d3ec81a
Prashant Ahire <33499614+prashantahire@users.noreply.github.com>
2019-01-05 23:30:20 -0800

    Create GraphColoringProblem.java

---
+++ src/my/algo/backtracking/GraphColoringProblem.java
@@ -0,0 +1,33 @@
+
+
+package my.algo.backtracking;
+
+
+public class GraphColoringProblem {
+
+    private static boolean isBoundingConditionSatisfied(int graph[][], int[] colors, int v, int clr) {
+        for (int i = 0; i < graph.length; i++)
+            if (graph[v][i] == 1 && clr == colors[i])
+                return false;
+        return true;
+    }
+
+    private static boolean graphColoringRec(int graph[][], int numberOfColors, int[] colors, int v) {
+
+        if (v == graph.length)
+            return true;
+        for (int c = 1; c <= numberOfColors; c++) {
+
+            if (isBoundingConditionSatisfied(graph, colors, v, c)) {
+                colors[v] = c;
+
+                if (graphColoringRec(graph, numberOfColors, colors, v + 1))
+                    return true;
+
+                colors[v] = 0;
+            }
+        }
+
+        return false;
+    }
+}
