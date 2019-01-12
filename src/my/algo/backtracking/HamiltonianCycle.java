e3480e903c380e8d77eb91045c9cff285df187cb
Prashant Ahire <33499614+prashantahire@users.noreply.github.com>
2019-01-11 18:21:09 -0800

    Update HamiltonianCycle.java

--- src/my/algo/backtracking/HamiltonianCycle.java
+++ src/my/algo/backtracking/HamiltonianCycle.java
@@ -42,5 +42,29 @@ public class HamiltonianCycle {
         return false;
 
     }
-
+    public static void findHamiltonianCycle(int graph[][]) {
+        
+        int path[] = new int[graph.length];
+        for(int i=0; i<graph.length; i++)
+            path[i]= -1;
+        
+        path[0] = 0;
+        System.out.println(isHamiltonianCycle(graph, path, 1));
+    }
+    public static void print(int arr[]) {
+        System.out.println("Solution....");
+        for (int i = 0; i < arr.length; i++)
+            System.out.print("    " + arr[i]);
+    }
+    
+    public static void main(String ...args) {
+        
+        int graph[][] = {{0, 1, 0, 1, 0}, 
+                {1, 0, 1, 1, 1}, 
+                {0, 1, 0, 0, 1}, 
+                {1, 1, 0, 0, 1}, 
+                {0, 1, 1, 1, 0}, 
+            }; 
+        findHamiltonianCycle(graph);
+    }
 }
