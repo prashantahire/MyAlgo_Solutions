3a3cdae64d79811a60a1ce188ac50583045b0d2e
Prashant Ahire <33499614+prashantahire@users.noreply.github.com>
2019-01-15 14:26:30 -0800

    Update NQueensProblem.java

--- src/my/algo/backtracking/NQueensProblem.java
+++ src/my/algo/backtracking/NQueensProblem.java
@@ -1,1 +1,51 @@
 
+
+package my.algo.backtracking;
+
+
+public class NQueensProblem {
+    
+    //Bounding condition check for back tracking
+    private static boolean isBoundingConditionSatisfied(int board[][], int row, int col) {
+
+        // row check
+        for (int i = 0; i < col; i++)
+            if (board[row][i] == 1)
+                return false;
+
+        // upper diagonal check
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 1)
+                return false;
+        }
+
+        // lower diagonal check
+        for (int i = row, j = col; j >= 0 && i < board.length; i++, j--) {
+            if (board[i][j] == 1)
+                return false;
+        }
+        return true;
+    }
+    
+    public static boolean solveNQUtil(int board[][], int col) {
+        if(col == board.length) {
+            print(board);
+            return true;
+        }
+        
+        boolean result = false;
+        for(int i=0; i<board.length; i++) {
+            
+            if(isBoundingConditionSatisfied(board, i, col)) {
+                
+                board[i][col] = 1;
+                
+                result = solveNQUtil(board, col+1) || result;
+
+                board[i][col] = 0;
+            }
+        }
+        return result;
+    }
+}
+
