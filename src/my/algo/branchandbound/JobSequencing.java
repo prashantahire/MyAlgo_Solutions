903c2d1ace5d70d866c2fcce825b6d599bc8808b
Prashant Ahire <33499614+prashantahire@users.noreply.github.com>
2019-01-17 16:28:23 -0800

    Update JobSequencing.java

--- src/my/algo/branchandbound/JobSequencing.java
+++ src/my/algo/branchandbound/JobSequencing.java
@@ -1,1 +1,39 @@
 
+
+package my.algo.branchandbound;
+
+
+import java.util.LinkedList;
+
+
+public class JobSequencing {
+
+    public static void  solve(int time[], int penalty[], int deadline[], int jobs) {
+
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        int cost[][] = new int[jobs][jobs];
+        int maxPenalty = Integer.MAX_VALUE;
+        int penaltysum[][] = new int[jobs][jobs];
+        int level = 0;
+        queue.add(0);
+        // while (!queue.isEmpty()) {
+        // int node = queue.poll();
+        int node = 0;
+        do {
+            for (int i = node; i < jobs; i++) {
+
+                cost[level][i] = getcost(penalty, i, level, cost);
+                penaltysum[level][i] = getpenalty(penalty, i, level, penaltysum);
+                if (penaltysum[level][i] < maxPenalty)
+                    maxPenalty = penaltysum[level][i];
+                if (cost[level][i] < maxPenalty)
+                    queue.add(i);
+            }
+            node = queue.poll();
+            level++;
+        } while (!queue.isEmpty());
+        System.out.println(maxPenalty);
+    }
+}
+
