e9814274f159b2c842184a1b520bd03bde29d4b3
Prashant Ahire <33499614+prashantahire@users.noreply.github.com>
2019-01-25 16:25:22 -0800

    Update SelectionSort.java

--- src/my/algo/solution/SelectionSort.java
+++ src/my/algo/solution/SelectionSort.java
@@ -1,1 +1,14 @@
-
+public class SelectionSort{
+    
+    public void flip(int arr[], int start, int k){        
+        int i= start;
+        int j = k-1;
+        while(i<j) {
+            int temp  = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
